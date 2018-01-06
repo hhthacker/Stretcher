b22a8fb52331b91c8213914590006073ba685f6f
Heather Thacker <hhthacker@gmail.com>
2018-01-05 21:30:35 -0600

    Update appconfig.js

--- Stretcher/App/appconfig.js
+++ Stretcher/App/appconfig.js
@@ -5,11 +5,6 @@
             templateUrl: "/App/Views/home.html",
             controller: "homeController"
         })
-        //.when("/login",
-        //{
-        //    templateUrl: "/App/Views/login.html",
-        //    controller: "loginController"
-        //})
         .when("/allgoals",
         {
             templateUrl: "/App/Views/allgoals.html",
@@ -20,7 +15,7 @@
             templateUrl: "/App/Views/newgoal.html",
             controller: "newGoalController"
         })
-        .when("/stretchgoal",
+        .when("/stretchgoal/:goalId",
         {
             templateUrl: "App/Views/stretchgoal.html",
             controller: "stretchGoalController"
@@ -36,27 +31,3 @@
             controller: "allReflectionsController"
         });
 }]);
-
-//app.run(["$rootScope", "$http", "$location", function ($rootScope, $http, $location) {
-
-//    $rootScope.isLoggedIn = function () { return !!sessionStorage.getItem("token"); };
-
-//    $rootScope.$on("$routeChangeStart", function (event, currRoute) {
-//        var anonymousPage = false;
-//        var originalPath = currRoute.originalPath;
-
-//        if (originalPath) {
-//            anonymousPage = originalPath.indexOf("/login") !== -1;
-//        }
-
-//        if (!anonymousPage && !$rootScope.isLoggedIn()) {
-//            event.preventDefault();
-//            $location.path("/login");
-//        }
-//    });
-
-//    var token = sessionStorage.getItem("token");
-
-//    if (token)
-//        $http.defaults.headers.common["Authorization"] = `bearer ${token}`;
-//}]);
